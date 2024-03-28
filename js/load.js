f5591f4852ad13b8b2b12b2e88cf6387f4b90543
dakimasu <dakimasuu@gmail.com>
2024-03-28 15:28:26 +0100

    Removed random background

--- js/load.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', function() {
-    document.body.style.backgroundImage = "url('/assets/images/background/" + (Math.floor(Math.random() * 5) + 1) + ".png')";
-    console.log('Finished loading!');
-});
