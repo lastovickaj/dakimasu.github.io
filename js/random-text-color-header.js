50997b627a956ab54e9e35ab0b44ef05e39dbb0b
dakimasu <dakimasuu@gmail.com>
2024-04-07 17:49:50 +0200

    Complete revamp once again! ;3

--- js/random-text-color-header.js
+++ js/random-text-color-header.js
@@ -1,5 +1,5 @@
 function changeColor() {
-    const colors = ['--Rosewater', '--Flamingo', '--Pink', '--Mauve', '--Red', '--Maroon', '--Peach', '--Yellow', '--Green', '--Teal', '--Sky', '--Sapphire', '--Blue', '--Lavender']; // List of color variables
+    const colors = ['--Rosewater', '--Flamingo', '--Pink', '--Mauve', '--Red', '--Maroon', '--Peach', '--Yellow', '--Green', '--Teal', '--Sky', '--Sapphire', '--Blue', '--Lavender'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
   
     const shadowWords = document.querySelectorAll('.shadow-words');
